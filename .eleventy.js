52b8ee3649b01cc1f144618c16240fa74d5d555f
Sebastian Sanitz <sebastian@sanitz.net>
2020-11-11 11:41:08 +0100

    Neue AboutMe Seite

--- .eleventy.js
+++ .eleventy.js
@@ -137,7 +137,7 @@ module.exports = function (eleventyConfig) {
 
   eleventyConfig.addFilter("readableDate", (dateObj) => {
     return DateTime.fromJSDate(dateObj, { zone: "utc" }).toFormat(
-      "dd LLL yyyy"
+      "dd.LL.yyyy"
     );
   });
 
